d7099bef7f6fffff806ba19c3720eba23d58de13
Mariam Boudrah <m.boudrah@interface3.be>
2023-03-09 15:42:28 +0100

    Update bis

--- EcoTravel_MB_ADO-Database/Table/Type_Logement.sql
+++ EcoTravel_MB_ADO-Database/Table/Type_Logement.sql
@@ -4,6 +4,5 @@
     [Description] NVARCHAR(50) NOT NULL, 
     [Type] NVARCHAR(50) NOT NULL, 
     CONSTRAINT [PK_Type_Logement] PRIMARY KEY ([idType]), 
-    CONSTRAINT [FK_Type_Logement_Logement] FOREIGN KEY ([Type]) REFERENCES [Logement]([Type]), 
     CONSTRAINT [UK_Type_Logement_Type] UNIQUE ([Type]) 
 )
