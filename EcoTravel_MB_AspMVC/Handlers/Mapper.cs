cea10f3fec329c37d3060a0e459ac43446a34b83
Mariam Boudrah <m.boudrah@interface3.be>
2023-03-09 16:18:00 +0100

    Create DAL-BLL-COMMON

---
+++ EcoTravel_MB_AspMVC/Handlers/Mapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcoTravel_MB_AspMVC.Handlers
+{
+    public class Mapper
+    {
+    }
+}
---
+++ EcoTravel_MB_AspMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EcoTravel_MB_AspMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
