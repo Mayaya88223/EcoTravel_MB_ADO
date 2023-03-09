e53c34909cb8948a7e857e0c496cb2bacc37c25e
Mariam Boudrah <m.boudrah@interface3.be>
2023-03-09 16:49:25 +0100

    Repository Common

---
+++ EcoTravel_MB-COMMON/Entities/IType_Logement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoTravel_MB_COMMON.Entities
+{
+    interface IType_Logement
+    {
+    }
+}
